281de304ddf567e98c9d8791520204fc7350b0f0
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2021-02-05 13:02:44 -0800

    Support for specifying the network interface to use to run a job
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

---
+++ internal/pkg/network/network.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package network
+
+// Config is the network configuration to use
+type Config struct {
+	// Device is the network ID to use to run application
+	Device string
+}
